721150c32de485f590ecbd4e0b6481f36efd4e27
Jan <okraskaj@ee.pw.edu.pl>
2017-03-08 11:33:44 +0100

    Update lamacz.c
    Bruteforce implementation and checkpassword moved to the same file.

--- lamacz.c
+++ lamacz.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+int checkpass(char *input){
+	char *check = "haslo";
+	return !(strcmp(check,input));	
+}
+
+void bruteImpl(char* str, int index, int maxDepth)
+{
+    for (int i = 0; i < alphabetSize; ++i){
+        str[index] = alphabet[i];
+        if (index == maxDepth - 1) printf("%s\n", str);
+        else bruteImpl(str, index + 1, maxDepth);
+    }
+}
+
+void bruteSequential(int maxLen){
+    char* buf = malloc(maxLen + 1);
+    for (int i = 1; i <= maxLen; ++i){
+        memset(buf, 0, maxLen + 1);
+        bruteImpl(buf, 0, i);
+    }
+    free(buf);
+}
+
 int main( int argc, char ** argv){
-    int i;
+    int i,j;
     int posInTab = 0;
     /* rozmiary
      10 - cyfry
@@ -10,7 +35,9 @@ int main( int argc, char ** argv){
      62 - calosc*/
     int wholeTabLength = 62;
     int bigtab[wholeTabLength];
-    
+    int passLen;	
+	passLen = atoi(argv[1]);
+        
     for(i=97; i<122; i++){
         /* male litery */
         printf("%i-%c\n",i,i);
@@ -30,10 +57,27 @@ int main( int argc, char ** argv){
         bigtab[posInTab] = i;
         posInTab++;
     }
-    for(i=0; i<wholeTabLength; i++){
+    
+    for(i=0; i<passLen; i++){
+    	for(j=0; j<=i; j++){
+    		printf("%c",bigtab[i]);
+		}
+	}
+    
+	if (checkpass("haslo")){
+		printf("dobre haslo");
+	} else{
+		printf("zle");
+	}
+	
+	
+	/*
+	system("%s %s", "./zahaslowany.exe", "haslo");
+	system("%s %s", "./zahaslowany.exe", "haslo");
+    
+	for(i=0; i<wholeTabLength; i++){
         printf("%c",bigtab[i]);
     }
-
-
-
+	*/
+    
 }
